cb8db907146d80fc5c749e30cf09aaa9f6350c0a
jiyun1006 <kimjiyoon1006@hanmail.net>
2022-01-06 23:48:30 +0900

    🎨 animation in component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
-  display : flex;
+const Wrapper = styled.div`
+  display: flex;
 `;
 
+const rotationAnimation = keyframes`
+  0%{
+    transform:rotato(0deg);
+    border-radius: 0px;
+  }50%{
+    transform:rotate(360deg);
+    border-radius: 100px;
+  }100%{
+    transform:rotate(0deg);
+    border-radius:
+    
+  }
+`
 
-const Input = styled.input.attrs({ required: true })`
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 80px;
+    }
+  }
+
+  
+
 `;
+
 
 function App() {
   return (
-    <Father>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>😄</span>
+      </Box>
+    </Wrapper >
+
   );
 }
 
