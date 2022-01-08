230ac81b3763e06e5faa6861267ea599e0f4bc90
jiyun1006 <kimjiyoon1006@hanmail.net>
2022-01-08 23:43:00 +0900

    🎨 optional props(feat. default props)

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
 
     <div>
-      <Circle bgColor="teal"/>
+      <Circle bgColor="teal" borderColor="tomato"/>
       <Circle bgColor="tomato"/>
 
    </div>
--- src/Circle.tsx
+++ src/Circle.tsx
@@ -3,23 +3,26 @@ import styled from "styled-components";
 
 interface ContainerProps{ 
     bgColor: string;
+    borderColor: string;
 }
 
 interface CircleProps {
     bgColor: string;
+    borderColor?: string;
 }
 
 
 const Container = styled.div<ContainerProps>`
     width: 200px;
     height: 200px;
-    background-color: ${(props) => props.bgColor};    
+    background-color: ${(props) => props.bgColor}; 
+    border: 1px solid ${(props) => props.borderColor};
     border-radius: 100px;
 `;
 
-function Circle({ bgColor } : CircleProps) {
+function Circle({ bgColor, borderColor } : CircleProps) {
 
-    return <Container bgColor={bgColor}/>;
+    return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} />;
 }
 
 
