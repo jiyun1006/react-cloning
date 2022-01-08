24f2f527529489bdb9781ef255e115c8865425e5
jiyun1006 <kimjiyoon1006@hanmail.net>
2022-01-09 02:17:44 +0900

    ⚡️ typing theme (feat. declare)

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+    bgColor: "white",
+    textColor: "balck",
+    btnColor: "tomato"
+}
+
+export const darkTheme: DefaultTheme = {
+    bgColor: "black",
+    textColor: "white",
+    btnColor: "tomato"
+
+}
